c1db4f5f4719574cd877811bfc861eefdcb05cd5
Fernando Paredes <nano@fdp.io>
2019-06-01 17:55:43 -0400

    Add a literal matcher parser

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,27 @@ struct Element {
     children: Vec<Element>,
 }
 
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn literal_parser() {
+        let parse_joe = match_literal("Hello Joe!");
+        assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+
+        assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+
     }
 }
