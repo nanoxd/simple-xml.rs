c44a8bb582b6e9ac1e42adfb4dc53151cac32aaf
Fernando Paredes <nano@fdp.io>
2019-06-01 17:55:56 -0400

    Add a parser for identifiers

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,28 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
         _ => Err(input),
     }
 }
+
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+    Ok((&input[next_index..], matched))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,6 +48,21 @@ mod tests {
         );
 
         assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
 
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
     }
 }
